16f1a5f5a21a0559e600b51a25cfa290ace711c9
dipeshkumarsah98 <dipeshsah98@gmail.com>
2022-09-28 22:22:51 +0545

    created store to fetch data

--- src/store/DogBreedStore.js
+++ src/store/DogBreedStore.js
@@ -3,27 +3,45 @@ import http from "../services/httpService";
 
 class DogBreedStore {
   breedList = [];
+  dogImageList = [];
   loading = true;
   error = "";
   constructor() {
     makeObservable(this, {
       breedList: observable,
+      dogImageList: observable,
       loading: observable,
       error: observable,
       getData: action,
+      getDogDetail: action,
     });
   }
   async getData() {
     try {
       const { data } = await http.get("https://dog.ceo/api/breeds/list/all");
-      console.log(data);
+      const breeds = Object.keys(data.message);
       runInAction(() => {
-        this.breedList = data;
+        this.breedList = breeds;
         this.loading = false;
         this.error = "";
       });
     } catch (error) {
       console.log("error::", error);
+    }
+  }
+  async getDogDetail(breed) {
+    try {
+      const { data } = await http.get(
+        `https://dog.ceo/api/breed/${breed}/images`
+      );
+      const dogs = data.message;
+      runInAction(() => {
+        this.dogImageList = dogs;
+        this.loading = false;
+        this.error = "";
+      });
+    } catch (error) {
+      console.log("errors:: ", error);
     }
   }
 }
